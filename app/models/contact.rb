421a0fc9f0d950c1d4d48f9332ad46553ee7b118
Joni Hell <joni.kristian.hell@gmail.com>
2017-03-23 09:59:47 +0000

    Improved error messages and reverted back model on new contact for later use

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-  validates :name, :comments, :email, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
